96b21727289ca7a98cd494a77473c20c069ccff2
KevinBrandao <kevenbrandaoms@outlook.com>
2022-01-31 21:28:04 -0300

    ni

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ClientController::class, 'create']);
 
-Route::post('/clients/store', [ClientController::class, 'store']);
+Route::post('/clients/store', [ClientController::class, 'store'
+]);
 
 Route::get('/clients/show/{client}',[ClientController::class, 'show']);
 
